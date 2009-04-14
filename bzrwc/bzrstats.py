dff469c1a09a801c9a9491ce716a9bb69d687486
Thomas Adamcik <adamcik@varulf.samfundet.no>
2009-04-14 21:26:59 +0200

    Use new VCS abstraction in get_bzr_stats

--- bzrwc/bzrstats.py
+++ bzrwc/bzrstats.py
@@ -1,71 +1,44 @@
-from datetime import datetime
-import bzrlib.branch
-import bzrlib.errors
 import re
 
 from bzrwc.models import Revision
+from bzrwc.vcs import Branch
 
 def get_bzr_stats(chart):
-    if chart.filter:
-        filter = re.compile(chart.filter)
-    else:
-        filter = None
-    if chart.exclude:
-        exclude = re.compile(chart.exclude)
-    else:
-        exclude = None
+    branch = Branch(chart.repository.url)
 
-    try:
-        branch = bzrlib.branch.Branch.open(chart.repository.url)
-    except bzrlib.errors.NotBranchError:
+    if not branch:
         return # Just use cached data
+
+    filter_re = re.compile(chart.filter or r'.')
+    exclude_re = re.compile(chart.exclude or r'^$')
+
+    file_filter = lambda file: (re.search(filter_re, file.name) and
+        not re.search(exclude_re, file.name))
 
     know_revisions = set(chart.revision_set.values_list('revision_id', flat=True))
 
-    for rev_id in branch.revision_history():
-        if rev_id in know_revisions:
-            continue # Revision is cached
+    for rev in branch.history:
+        if rev.id in know_revisions:
+            continue
 
-        num_lines, num_words, num_chars, num_bytes = 0, 0, 0, 0
-
-        rev_no = branch.revision_id_to_revno(rev_id)
-        rev = branch.repository.get_revision(rev_id)
-        timestamp = datetime.fromtimestamp(rev.timestamp)
-        revtree = branch.repository.revision_tree(rev_id)
-
-        for file in revtree.list_files():
-            file_name = file[0]
-            file_id = file[3]
-
-            # Skip file if matching exclude
-            if exclude is not None and re.search(exclude, file_name):
-                continue
-            # Skip file if not matching filter
-            if filter is not None and not re.search(filter, file_name):
-                continue
-
-            revtree.lock_read()
-            try:
-                for line in revtree.get_file_lines(file_id):
-                    if len(line.strip()) == 0:
-                        continue
-                    num_lines += 1
-                    # Split on all whitespace
-                    num_words += len(line.split(None))
-                    num_chars += len(line)
-            finally:
-                revtree.unlock()
-            bytes = revtree.get_file_size(file_id)
-            if bytes is not None:
-                num_bytes += bytes
-
-        revision = Revision(
-            revision_id=rev_id,
-            revision_no=rev_no,
-            timestamp=timestamp,
-            num_lines=num_lines,
-            num_words=num_words,
-            num_chars=num_chars,
-            num_bytes=num_bytes)
-
+        revision = get_revision_stats(rev, file_filter)
         chart.revision_set.add(revision)
+
+
+def get_revision_stats(rev, filter_function):
+    num_lines, num_words, num_chars, num_bytes = 0, 0, 0, 0
+
+    for file in filter(filter_function, rev.files):
+        num_lines += file.lines
+        num_words += file.words
+        num_chars += file.chars
+        num_bytes += file.chars
+
+    return Revision(
+        revision_id=rev.id,
+        revision_no=rev.no,
+        timestamp=rev.timestamp,
+        num_lines=num_lines,
+        num_words=num_words,
+        num_chars=num_chars,
+        num_bytes=num_bytes)
