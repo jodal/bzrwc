4afab0e71df8f4bf98e30491e19a6368af920d24
Thomas Adamcik <adamcik@varulf.samfundet.no>
2009-04-15 00:44:06 +0800

    Add some whitespace to bzrstats.py
    Signed-off-by: Stein Magnus Jodal <stein.magnus@jodal.no>

--- bzrwc/bzrstats.py
+++ bzrwc/bzrstats.py
@@ -14,6 +14,7 @@ def get_bzr_stats(chart):
         exclude = re.compile(chart.exclude)
     else:
         exclude = None
+
     try:
         branch = bzrlib.branch.Branch.open(chart.repository.url)
     except bzrlib.errors.NotBranchError:
@@ -21,20 +22,25 @@ def get_bzr_stats(chart):
     for rev_id in branch.revision_history():
         if chart.revision_set.filter(revision_id=rev_id).count():
             continue # Revision is cached
+
         num_lines, num_words, num_chars, num_bytes = 0, 0, 0, 0
+
         rev_no = branch.revision_id_to_revno(rev_id)
         rev = branch.repository.get_revision(rev_id)
         timestamp = datetime.fromtimestamp(rev.timestamp)
         revtree = branch.repository.revision_tree(rev_id)
+
         for file in revtree.list_files():
             file_name = file[0]
             file_id = file[3]
+
             # Skip file if matching exclude
             if exclude is not None and re.match(exclude, file_name) is not None:
                 continue
             # Skip file if not matching filter
             if filter is not None and re.match(filter, file_name) is None:
                 continue
+
             revtree.lock_read()
             try:
                 for line in revtree.get_file_lines(file_id):
@@ -58,4 +64,5 @@ def get_bzr_stats(chart):
             num_words=num_words,
             num_chars=num_chars,
             num_bytes=num_bytes)
+
         chart.revision_set.add(revision)
