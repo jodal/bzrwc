11450f218fac9a4962953de59dcb47b87c4f364c
Thomas Adamcik <adamcik@varulf.samfundet.no>
2009-04-16 05:07:16 +0200

    Add crude diffstat to revisions

--- bzrwc/vcs.py
+++ bzrwc/vcs.py
@@ -1,7 +1,9 @@
 from datetime import datetime
 
+import re
 import bzrlib.branch
 import bzrlib.errors
+from bzrlib.diff import show_diff_trees
 
 class Branch(object):
     def __init__(self, url):
@@ -15,20 +17,34 @@ class Branch(object):
 
     @property
     def history(self):
+        prev_id = None
         for rev_id in self.branch.revision_history():
-            yield Revision(self.branch, rev_id)
+            yield Revision(self.branch, rev_id, prev_id)
+            prev_id = rev_id
 
 class Revision(object):
-    def __init__(self, branch, rev_id):
+    def __init__(self, branch, rev_id, prev_id):
         self.branch = branch
         self.rev_id = rev_id
+        self.prev_id = prev_id
         self._revtree = None
+        self._prev_revtree = None
+
+        self._diffstat_run = False
+        self._additions = 0
+        self._deletions = 0
 
     @property
     def revtree(self):
         if not self._revtree:
             self._revtree = self.branch.repository.revision_tree(self.rev_id)
         return self._revtree
+
+    @property
+    def prev_revtree(self):
+        if not self._prev_revtree:
+            self._prev_revtree = self.branch.repository.revision_tree(self.prev_id)
+        return self._prev_revtree
 
     @property
     def id(self):
@@ -55,6 +71,28 @@ class Revision(object):
             file_id = file[3]
 
             yield File(self.revtree, file_name, file_id)
+
+    @property
+    def additions(self):
+        if not self._diffstat_run:
+            self._diffstat()
+
+        return self._additions
+
+    @property
+    def deletions(self):
+        if not self._diffstat_run:
+            self._diffstat()
+
+        return self._deletions
+
+    def _diffstat(self):
+        s = DiffStat()
+        show_diff_trees(self.prev_revtree, self.revtree, s)
+
+        self._additions = s.additions
+        self._deletions = s.deletions
+        self._diffstat_run = True
 
 class File(object):
     def __init__(self, revtree, file_name, file_id):
@@ -89,3 +127,20 @@ class File(object):
             self.revtree.unlock()
 
         return lines
+
+class DiffStat:
+    addition_re = re.compile(r'^\+[^+]')
+    deletion_re = re.compile(r'^\-[^-]')
+
+    def __init__(self):
+        self.additions = 0
+        self.deletions = 0
+
+    def write(self, diff_string):
+        for line in diff_string.split('\n'):
+            if re.search(self.addition_re, line):
+                self.additions += 1
+            elif re.search(self.deletion_re, line):
+                self.deletions += 1
+
+        print diff_string
