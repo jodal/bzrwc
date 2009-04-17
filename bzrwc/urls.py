c2facd71a9d1c95ee016cfe8615467cd91e2c9bb
Thomas Adamcik <adamcik@varulf.samfundet.no>
2009-04-18 01:58:59 +0200

    Add chart view to urls

--- bzrwc/urls.py
+++ bzrwc/urls.py
@@ -7,4 +7,6 @@ urlpatterns = patterns('bzrwc.views',
         name='repository-list-user'),
     url(r'^(?P<username>[0-9A-Za-z-_]+)/(?P<repository_slug>[0-9a-z-_]+)/$',
         'repository_details', name='repository-details'),
+    url(r'^(?P<username>[0-9A-Za-z-_]+)/(?P<repository_slug>[0-9a-z-_]+)/(?P<chart_slug>[0-9a-z-_]+)/$',
+        'chart', name='chart'),
 )
